57cfa2264f1fcaf38b68efeea988d77c82f6e10a
R.S. Richards <RobertoRichards2@Robertos-MacBook-Pro.local>
2016-05-26 13:54:15 -0400

    Checkpoint 3 Work

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,16 +3,11 @@
     this.rooms = ['Room 1', 'Room 2', 'Room 3'];
     this.modalRoom = function() {
       console.log("Clicked in modal room");
-    }
-        this.open = function (){
-          var modalInstance = $uibModal.open({
-            controller: 'ModalCtrl as modal',
-            templateUrl: '/templates/modal.html'
-          });
-        };
+    };
+
   }
 
   angular
       .module('blocChat')
-      .controller('HomeCtrl', ['$uibModal', '$scope', HomeCtrl]);
+      .controller('HomeCtrl', HomeCtrl);
 })();
